d0875b5018604b4204fc1ab96490cb06d18b1dc7
Garrett Bluma <garrett.bluma@gmail.com>
2011-11-25 23:33:22 -0600

    Added function to create inverted-normal cubes. (for skyboxes)

--- Cube.hs
+++ Cube.hs
@@ -47,3 +47,31 @@ drawCube size = do
       drawFace n e a b f
       drawFace o g c d h
 
+-------------------------------------------------------------
+drawInvertedCube :: GLfloat -> IO ()
+drawInvertedCube size = do
+    let a = Vertex3   size    size    size
+        b = Vertex3   size    size  (-size)
+        c = Vertex3   size  (-size) (-size)
+        d = Vertex3   size  (-size)   size
+        e = Vertex3 (-size)   size    size
+        f = Vertex3 (-size)   size  (-size)
+        g = Vertex3 (-size) (-size) (-size)
+        h = Vertex3 (-size) (-size)   size
+
+        i = Normal3 (-1)   0    0
+        k = Normal3   1    0    0
+        l = Normal3   0    0    1
+        m = Normal3   0    0    1
+        n = Normal3   0  (-1)   0
+        o = Normal3   0    1    0
+
+
+    renderPrimitive Quads $ do
+      drawFace i d c b a
+      drawFace k g h e f
+      drawFace l c g f b
+      drawFace m h d a e
+      drawFace n e a b f
+      drawFace o g c d h
+
